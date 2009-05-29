63dc47cc9a4a0682059ffe1c8d821790297208c2
Hallison Batista <email@hallisonbatista.com>
2009-05-29 13:42:35 -0400

    Parsed index.liquid to index.erb.

--- javascripts/print.js
+++ javascripts/print.js
@@ -1,10 +1,10 @@
 /**
- * Theme Name  : Clearly
- * Theme URI   : http://hallison.batista.nom.br/projetos/clearly
+ * Theme Name  : Weblooks
+ * Theme URI   : http://hallisonbatista.com
  * Description : JavaScript file with useful functions.
- * Version     : 0.1
+ * Version     : 0.1.0
  * Author      : Hallison Batista
- * Author URI  : http://hallison.batista.nom.br
+ * Author URI  : http://hallisonbatista.com
  *
  * References
  * 
